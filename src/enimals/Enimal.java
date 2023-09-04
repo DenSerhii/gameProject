cfd6416c13f886b3cd52fb8f3fe1433af2a9275c
Svitlana <iskiv_s@ukr.net>
2023-09-04 18:01:17 +0300

    add  class Herbivorous

--- src/enimals/Enimal.java
+++ src/enimals/Enimal.java
@@ -1,5 +1,18 @@
 package enimals;
 
-public class Enimal {
+public abstract class Enimal {
 
+    public void eat(){
+
+}
+    public void move(){
+
+    }
+    public void reproduce(){
+
+    }
+    public void death(){
+
+    }
+    public abstract void run();
 }
---
+++ src/herbivorous/Herbivorous.java
@@ -0,0 +1,54 @@
+package herbivorous;
+
+import enimals.Enimal;
+
+public class Herbivorous extends Enimal {
+    private String name;
+    private double weight;
+    private double kilogramsOfFoodForCompleteSaturation;
+    private int speedMove;
+
+    public Herbivorous(String name, double weight, double kilogramsOfFoodForCompleteSaturation, int speedMove) {
+        this.name = name;
+        this.weight = weight;
+        this.kilogramsOfFoodForCompleteSaturation = kilogramsOfFoodForCompleteSaturation;
+        this.speedMove = speedMove;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getKilogramsOfFoodForCompleteSaturation() {
+        return kilogramsOfFoodForCompleteSaturation;
+    }
+
+    public void setKilogramsOfFoodForCompleteSaturation(double kilogramsOfFoodForCompleteSaturation) {
+        this.kilogramsOfFoodForCompleteSaturation = kilogramsOfFoodForCompleteSaturation;
+    }
+
+    public int getSpeedMove() {
+        return speedMove;
+    }
+
+    public void setSpeedMove(int speedMove) {
+        this.speedMove = speedMove;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
